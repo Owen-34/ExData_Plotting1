175d669fbca43f70649efea232691c3c73adff84
Owen-34 <75723539+Owen-34@users.noreply.github.com>
2020-12-21 12:59:51 +0200

    Create Loading_and_cleaning_table.R
    Loading and cleaning the table

---
+++ Loading_and_cleaning_table.R
@@ -0,0 +1,2 @@
+## Loading and Cleaning the Table
+t <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
